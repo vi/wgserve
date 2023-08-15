12a4ad115ed83fa0970e927169415e42a3baf8a1
Vitaly _Vi Shukela <vi0oss@gmail.com>
2023-08-15 04:49:13 +0200

    Empty app template with branding

---
+++ app/src/main/java/org/vi_server/wgserver/MainActivity.java
@@ -0,0 +1,14 @@
+package org.vi_server.wgserver;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
